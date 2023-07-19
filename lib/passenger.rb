7737081ae7b9e35099fae1d67a55cad2ca2e697d
Will Weston <Williamweston81@gmail.com>
2023-07-19 10:41:41 -0500

    Iteration 2 complete pass

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -9,11 +9,7 @@ class Passenger
   end
   
   def adult?
-    if @age >= 18 
-      true
-    else
-      false
-    end
+    @age >= 18 
   end
 
   def driver?
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -5,11 +5,45 @@ RSpec.describe Vehicle do
   before(:each) do
     @vehicle = Vehicle.new("2001", "Honda", "Civic")
     @charlie = Passenger.new({name: "Charlie", age: 18})
-    @jude = Passenger.new({"name" => "Jude", "age" => 20})  
+    @jude = Passenger.new({name: "Jude", age: 20})  
     @taylor = Passenger.new({name: "Taylor", age: 12})  
   end
 
   it "exists" do
     expect(@vehicle).to be_instance_of(Vehicle)
+  end
+
+  it "has attributes" do
+    expect(@vehicle.year).to eq("2001")
+    expect(@vehicle.make).to eq("Honda")
+    expect(@vehicle.model).to eq("Civic")
+  end
+
+  it "has speeding? set to false" do
+    expect(@vehicle.speeding?).to eq(false)
+
+    @vehicle.speed
+
+    expect(@vehicle.speeding?).to eq(true)
+  end
+
+  it "has empty passengers array" do
+    expect(@vehicle.passengers).to eq([])
+  end
+
+  it "adds passengers" do
+    @vehicle.add_passenger(@charlie)
+    @vehicle.add_passenger(@jude)
+    @vehicle.add_passenger(@taylor)
+
+    expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
+  end
+
+  it "counts number of adults in vehicle" do
+    @vehicle.add_passenger(@charlie)
+    @vehicle.add_passenger(@jude)
+    @vehicle.add_passenger(@taylor)
+
+    expect(@vehicle.num_adults).to eq(2)
   end
 end
